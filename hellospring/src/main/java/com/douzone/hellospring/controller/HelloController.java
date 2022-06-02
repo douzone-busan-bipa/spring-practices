460edd348aded8db69e44d899b232ea310d70e0a
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-06-02 15:29:56 +0900

    hellospring #2

--- hellospring/src/main/java/com/douzone/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/douzone/hellospring/controller/HelloController.java
@@ -5,8 +5,16 @@ import org.springframework.web.bind.annotation.RequestMapping;
 
 @Controller
 public class HelloController {
+	
 	@RequestMapping("/hello") 
 	public String hello() {
 		return "/WEB-INF/views/hello.jsp";
 	}
+
+	@RequestMapping("/hello2") 
+	public String hello2(String name) {
+		System.out.println(name);
+		return "/WEB-INF/views/hello2.jsp";
+	}
+	
 }
