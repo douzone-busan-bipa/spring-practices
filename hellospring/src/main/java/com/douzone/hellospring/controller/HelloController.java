4176c33e9d033be79377c6898bd2ff4c495ca598
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-06-02 16:15:06 +0900

    hellospring #3

--- hellospring/src/main/java/com/douzone/hellospring/controller/HelloController.java
+++ hellospring/src/main/java/com/douzone/hellospring/controller/HelloController.java
@@ -1,7 +1,17 @@
 package com.douzone.hellospring.controller;
 
+import java.io.IOException;
+import java.io.Writer;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpSession;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+import org.springframework.web.servlet.ModelAndView;
 
 @Controller
 public class HelloController {
@@ -16,5 +26,40 @@ public class HelloController {
 		System.out.println(name);
 		return "/WEB-INF/views/hello2.jsp";
 	}
+
+	@RequestMapping("/hello3") 
+	public ModelAndView hello3(String name) {
+		ModelAndView mav = new ModelAndView();
+		mav.addObject("name", name);
+		mav.setViewName("/WEB-INF/views/hello3.jsp");
+		
+		return mav;
+	}
+
+	@RequestMapping("/hello4") 
+	public String hello4(String name, Model model) {
+		model.addAttribute("name", name);
+		return "/WEB-INF/views/hello4.jsp";
+	}
 	
+	@ResponseBody
+	@RequestMapping("/hello5") 
+	public String hello5() {
+		return "<h1>Hello World</h1>";
+	}
+	
+	@RequestMapping("/hello6") 
+	public String hello6() {
+		return "redirect:/hello";
+	}
+	
+	/* 비추!!!!! 기술비침투 전력 위배*/
+	@RequestMapping("/hello7") 
+	public void hello7(
+		HttpServletRequest request,
+		HttpServletResponse response,
+		HttpSession session,
+		Writer pw) throws IOException {
+		pw.write("<h1>Hello World</h1>");
+	}
 }
