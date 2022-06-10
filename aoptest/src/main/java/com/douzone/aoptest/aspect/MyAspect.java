1fbb59d795d70cd9644ec56982ac2504b2ec6d65
douzone-busan-bipa <douzone.busan.bipa@gmail.com>
2022-06-10 12:07:21 +0900

    AOP test #finish

--- aoptest/src/main/java/com/douzone/aoptest/aspect/MyAspect.java
+++ aoptest/src/main/java/com/douzone/aoptest/aspect/MyAspect.java
@@ -1,5 +1,10 @@
 package com.douzone.aoptest.aspect;
 
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.After;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.AfterThrowing;
+import org.aspectj.lang.annotation.Around;
 import org.aspectj.lang.annotation.Aspect;
 import org.aspectj.lang.annotation.Before;
 import org.springframework.stereotype.Component;
@@ -12,4 +17,34 @@ public class MyAspect {
 	public void beforeAdvice() {
 		System.out.println("---Before Advice---");
 	}
+	
+	@After("execution(* *..*.ProductService.find(..))")
+	public void afterAdvice() {
+		System.out.println("---After Advice---");
+	}
+
+	@AfterReturning("execution(* *..*.ProductService.*(..))")
+	public void afterReturningAdvice() {
+		System.out.println("---After Returning Advice---");
+	}
+
+	@AfterThrowing(value="execution(* *..*.*.*(..))", throwing="ex")
+	public void afterThrowingAdvice(Throwable ex) {
+		System.out.println("---After Throwing Advice:" + ex + "---");
+	}
+	
+	@Around("execution(* *..*.ProductService.*(..))")
+	public Object aroundAdvice(ProceedingJoinPoint pjp) throws Throwable {
+		/* before */
+		System.out.println("---Around(Before) Advice---");
+		
+		// PoinCut 메소드 실행
+		Object result = pjp.proceed();
+		
+		/* after */
+		System.out.println("---Around(After) Advice---");
+		
+		return result;
+	}
+	
 }
